18971350788665685e8edee5e5e1715bebf23173
Cle-ParkBomin <114986489+springin98@users.noreply.github.com>
2025-05-22 14:29:30 +0900

    doc: tailwindcss  정렬

--- src/components/modal/ProgressModal.tsx
+++ src/components/modal/ProgressModal.tsx
@@ -42,9 +42,9 @@ export default function ProgressModal({
 
   if (!visible) return null;
   return (
-    <div className='fixed top-0 right-0 bottom-0 left-0 z-20 bg-grey-950/40'>
+    <div className='bg-grey-950/40 fixed bottom-0 left-0 right-0 top-0 z-20'>
       <div
-        className='absolute top-1/2 left-1/2 flex flex-1 -translate-x-1/2 -translate-y-1/2 transform items-center justify-between gap-10 rounded-lg border border-grey-700 bg-grey-0 px-8 py-6 shadow-strong'
+        className='border-grey-700 bg-grey-0 shadow-strong absolute left-1/2 top-1/2 flex flex-1 -translate-x-1/2 -translate-y-1/2 transform items-center justify-between gap-10 rounded-lg border px-8 py-6'
         ref={modalRef}
       >
         <div className='flex flex-col gap-4'>
@@ -54,9 +54,9 @@ export default function ProgressModal({
               ({count}/{total})
             </p>
           </div>
-          <div className='relative flex h-1.5 w-[416px] rounded-full bg-grey-300'>
+          <div className='bg-grey-300 relative flex h-1.5 w-[416px] rounded-full'>
             <div
-              className={`absolute top-0 left-0 z-10 h-1.5 rounded-full bg-primary-500`}
+              className={`bg-primary-500 absolute left-0 top-0 h-1.5 rounded-full`}
               style={{ width: `${416 * (count / total)}px` }}
             />
           </div>
