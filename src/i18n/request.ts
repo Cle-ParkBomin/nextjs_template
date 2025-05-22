f8a64f59d052663219d25cb4cf5ac7d172cc6387
Cle-ParkBomin <114986489+springin98@users.noreply.github.com>
2025-05-22 13:42:35 +0900

    feature: 다국어 라이브러리 추가 next-intl

---
+++ src/i18n/request.ts
@@ -0,0 +1,20 @@
+import { hasLocale } from 'next-intl';
+import { getRequestConfig } from 'next-intl/server';
+import { routing } from './routing';
+
+interface Messages {
+  [key: string]: string | Messages;
+}
+
+export default getRequestConfig(async ({ requestLocale }) => {
+  const requested = await requestLocale;
+  const locale = hasLocale(routing.locales, requested) ? requested : routing.defaultLocale;
+
+  const messages = ((await import(`../../messages/${locale}.json`)) as { default: Messages })
+    .default;
+
+  return {
+    locale,
+    messages,
+  };
+});
